6f00a045525f2ea8b34ceb70368d9af90b5de6fb
ScaryNuggets <144059122+ScaryNuggets@users.noreply.github.com>
2023-10-04 19:25:56 +0300

    configurations.py

--- configurations.py
+++ configurations.py
@@ -1,5 +1,5 @@
 # Необходимые адреса для направления запросов
-URL_SERVICE = "https://f10da5d6-fdd1-4366-bdd1-70b8ac356263.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://593b40a6-2ad8-4b72-94a2-a420c53d7007.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_USER_ORDER = "/api/v1/orders"
-GETS_NEW_ORDER = "/api/v1/orders/track?t="
+GETS_NEW_ORDER = "/api/v1/orders/track"
